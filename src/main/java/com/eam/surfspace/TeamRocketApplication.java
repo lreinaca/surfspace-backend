efc39c9c7151ba11ba1c2abe20ff3451d7022ea3
LorenaReina <lorenareina2016@gmail.com>
2025-09-09 16:40:38 -0500

    Initial commit

---
+++ src/main/java/com/eam/surfspace/TeamRocketApplication.java
@@ -0,0 +1,13 @@
+package com.eam.surfspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamRocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamRocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eam/surfspace/TeamRocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eam.surfspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamRocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
